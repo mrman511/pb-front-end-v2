b68a34e786e44504f6b5d8ec3c42042495ed29dc
Paul Bodner <bodner80@gmail.com>
2022-08-01 17:50:01 -0700

    mobile stying including menu animations, media queries, and refactored projectList arrows to change viewed project

--- paulbodner-2/src/App.js
+++ paulbodner-2/src/App.js
@@ -1,16 +1,30 @@
 import './App.scss';
+import './components/styles/menu.scss'
+import React, { useState, useEffect } from 'react';
 import Banner from './components/Banner';
 import Menu from './components/Menu';
 import Fullpage from './components/FullPage';
 
 
 
+
 function App() {
+  const [ showMenu, setShowMenu ] = useState(false);
+
+  const toggleMenu = () => {
+    if (showMenu) {
+      setShowMenu(false)
+    } else {
+      setShowMenu(true)
+    }
+    console.log('TOGGLE MENU', showMenu);
+  }
+
   return (
     <>
       <div className='page'>
-        <Banner />
-        <Menu />
+        <Banner toggleMenu = { toggleMenu } showMenu = { showMenu } />
+        <Menu  showMenu={ showMenu }/>
       </div>
       <Fullpage />
     </>
